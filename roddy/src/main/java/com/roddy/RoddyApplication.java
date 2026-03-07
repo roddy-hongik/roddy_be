b157c88090c120717706d91c76b6602b6c0b03df
whc9999 <whc3377@gmail.com>
2026-03-07 19:22:08 +0900

    chore: Spring Boot 초기 프로젝트 세팅 및 MySQL 연동

---
+++ roddy/src/main/java/com/roddy/RoddyApplication.java
@@ -0,0 +1,13 @@
+package com.roddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoddyApplication.class, args);
+	}
+
+}
---
+++ roddy/src/test/java/com/roddy/RoddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
