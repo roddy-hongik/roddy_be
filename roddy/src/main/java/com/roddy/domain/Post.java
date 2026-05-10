781839e1267a5cc96c76c7c7f07a1c0573753018
박서영 <emilypark0721@gmail.com>
2026-05-10 23:24:47 +0900

    [Fix] Post 엔티티 update 로직 수정

--- roddy/src/main/java/com/roddy/domain/Post.java
+++ roddy/src/main/java/com/roddy/domain/Post.java
@@ -89,6 +89,13 @@ public class Post extends BaseEntity{
         if (title == null || title.isBlank()) {
             throw new IllegalArgumentException("제목은 필수입니다.");
         }
+
+        if (content == null || content.isBlank()) {
+                   throw new IllegalArgumentException("내용은 필수입니다.");
+              }
+          if (category == null) {
+              throw new IllegalArgumentException("카테고리는 필수입니다.");
+              }
         this.title = title;
         this.content = content;
         this.category = category;
